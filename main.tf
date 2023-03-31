e814d1a64e728aa0b9214c0a5f9b304b639e5bcb
Douglass Coronado <dcoronado@funio.com>
2023-03-30 22:20:29 -0400

    FIRESTORE: create project, enable firestore service, and create firestore

--- main.tf
+++ main.tf
@@ -1,14 +1,34 @@
 #readme
 #CREATE A FIRESTORE NATIVE DATABASE
 provider "google" {
-  project	= "montreal-project-mcit"
   region	= "northamerica-northeast1"
   zone		= "northamerica-northeast1-a"
 }
 
+resource "google_project" "project" {
+  project_id	= "project-firestore-mtl-%{random_suffix}"
+  name		= "project-firestore-mtl"
+}
+
+resource "time_sleep" "wait_60_seconds" {
+  depends_on	= [google_project.project]
+  create_duration = "60s"
+}
+
+resource "gogle_project_service" "firestore" {
+  project 	= google_project.project.project_id
+  service 	= "firestore.googleapis.com"
+
+  depends_on 	= [time_sleep.wait_60_seconds]
+}
+
 resource "google_firestore_database" "database" {
-  project     = "montreal-project-mcit"
-  name        = "mtl-firestore"
-  location_id = "northamerica-northeast1"
-  type        = "FIRESTORE_NATIVE"
+  project     			= "google_project.project.project_id"
+  name        			= "(default)"
+  location_id 			= "northamerica-northeast1"
+  type        			= "FIRESTORE_NATIVE"
+  concurrency_mode		= "OPTIMISTIC"
+  app_engine_integration_mode 	= "DISABLED"
+
+  depends_on 			= [google_project_service.firestore]
 }
